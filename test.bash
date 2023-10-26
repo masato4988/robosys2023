8335c5093cc12ef0c2cb821301475fcf53465fd9
Aita Masato <masato10030809@gmail.com>
2023-10-26 12:53:19 +0900

    Add test casese

--- test.bash
+++ test.bash
@@ -4,8 +4,12 @@ ng () {
 	echo NG at Line $1
 	res=1
 }
+
 res=0
 
-out=(seq 5 | ./plus)
-[ "${out}" = 15 ]
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LNENO}
+[ "$res" = 0 ] && echo OK
+exit $res
 
