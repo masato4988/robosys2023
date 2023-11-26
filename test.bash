f9f4d43346838e391a80144cb41eeb7bc4b7385b
Aita Masato <masato10030809@gmail.com>
2023-11-26 15:59:04 +0900

    Add test for multiply to test.bash

--- test.bash
+++ test.bash
@@ -9,6 +9,7 @@ ng () {
 
 res=0
 
+### plus ###
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
@@ -21,6 +22,24 @@ out=$(echo あ | ./plus)
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+### multiply ###
+### I/O TEST ###
+out=$(seq 5 | ./multiply)
+[ "${out}" = 120 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./multiply)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
